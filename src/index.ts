3a964ce35dd08166a07d661575457547828dac6f
Jonas <Jonas>
2020-06-29 19:53:06 +0200

    add public dir

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,26 @@ const ac = new ApiController()
 const s = serve({ port: 3000 })
 console.log("http://localhost:3000/")
 
-for await (const req of s) {
 
-    switch (req.url) {
-        case "/api":
-            if(req.method == "GET") {
-                await ac.HandleDownload(req)
-            }
-            else if(req.method == "POST") {
-                await ac.HandleUpload(req)
-            } else {
-                await req.respond({status: 405})
-            }
-            break;
-            
-        default:
-            await req.respond({status: 404})
-            break;
-    }
-    
+for await (const req of s) {
+  console.log(`${req.method} ${req.url}`)
+  switch (req.url) {
+    case "/api":
+      if(req.method == "GET") {
+        await ac.HandleDownload(req)
+      }
+      else if(req.method == "POST") {
+        await ac.HandleUpload(req)
+      } else {
+        await req.respond({status: 405})
+      }
+      break
+           
+    case "/":
+      await req.respond({status: 200, body: await Deno.open('./public/index.html')})
+      break
+    default:
+      await req.respond({status: 404})
+      break;
+  }
 }
